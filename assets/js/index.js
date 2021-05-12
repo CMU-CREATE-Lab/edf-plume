4a6d29dc8af138bdc9547e3fff577227156b8947
Paul Dille <pdille@cmucreatelab.org>
2021-05-12 16:10:53 -0400

    - Update gmaps-utils submodule

--- assets/js/index.js
+++ assets/js/index.js
@@ -1355,6 +1355,7 @@ function createAndShowSensorMarker(data, epochtime_milisec, is_current_day, info
   return new CustomMapMarker({
     "type": getSensorType(info),
     "sensor_type" : info['marker_type'],
+    "marker_icon_size" : info['marker_type'] == "purple_air" ? 15 : null,
     "data": parseSensorMarkerData(data, is_current_day, info),
     "click": function (marker) {
       selectedSensorMarker = marker;
